9a0c84a9c06ab85eda2a9e84910a79d21d5fbf6f
killvung <kiulam125@gmail.com>
2016-07-24 18:47:55 -0500

    Create 2 for loops to extract data bit by bit

--- tutorial/spiders/afl_wiki_spider.py
+++ tutorial/spiders/afl_wiki_spider.py
@@ -10,6 +10,11 @@ class AFL_wiki_Spider(scrapy.Spider):
         sel = response.xpath('//table[preceding-sibling::h3/span[@id="Current_clubs"] and following-sibling::h3/span[@id="Former_clubs"]]')[0]
         #This would point toward the table below Current clubs and above former clubs
         print("--------------------------------")
-        for row in sel.xpath('tr'):
-            print(row.extract())
+        for club_selector_table_row in sel.xpath('tr'):
+            
+            for club_selectory_information in club_selector_table_row.xpath('*'):
+
+                print(club_selectory_information)
+
+
         print("--------------------------------")
