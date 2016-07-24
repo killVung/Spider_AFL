c7cf6cc9866c8473ab780306a08b9bf8888d6c67
killvung <kiulam125@gmail.com>
2016-07-24 18:33:04 -0500

    Find a specific way to mention the able I want

--- tutorial/spiders/afl_wiki_spider.py
+++ tutorial/spiders/afl_wiki_spider.py
@@ -7,10 +7,9 @@ class AFL_wiki_Spider(scrapy.Spider):
         "https://en.wikipedia.org/wiki/Australian_Football_League#Current_clubs"
     ]
     def parse(self,response):
-        sel = response.xpath('//table[preceding-sibling::h3]')[0]
+        sel = response.xpath('//table[preceding-sibling::h3/span[@id="Current_clubs"] and following-sibling::h3/span[@id="Former_clubs"]]')[0]
+        #This would point toward the table below Current clubs and above former clubs
         print("--------------------------------")
-
-
         for row in sel.xpath('tr'):
             print(row.extract())
         print("--------------------------------")
