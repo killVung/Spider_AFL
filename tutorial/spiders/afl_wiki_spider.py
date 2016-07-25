aa18dafa25848c668273a4cb0d097a5712e78c5b
killvung <kiulam125@gmail.com>
2016-07-24 22:04:03 -0500

    Return item with 4 attributes

--- tutorial/spiders/afl_wiki_spider.py
+++ tutorial/spiders/afl_wiki_spider.py
@@ -10,11 +10,29 @@ class AFL_wiki_Spider(scrapy.Spider):
         sel = response.xpath('//table[preceding-sibling::h3/span[@id="Current_clubs"] and following-sibling::h3/span[@id="Former_clubs"]]')[0]
         #This would point toward the table below Current clubs and above former clubs
         print("--------------------------------")
-        for club_selector_table_row in sel.xpath('tr'):
-            
-            for club_selectory_information in club_selector_table_row.xpath('*'):
+        #Specify the tables row of the football clubs
+        for club_selector_table_row in sel.xpath('tr')[1:len(sel.xpath('tr'))-1]:
+            item = AFLItem()
+            club_selector_information = club_selector_table_row.xpath('*')
 
-                print(club_selectory_information)
+            club_name_selector = club_selector_information[0]
+            club_name = club_name_selector.xpath('a/@title')[0].extract()
+            item['name'] = club_name
+
+            club_state_selector = club_selector_information[2]
+            club_state = club_state_selector.xpath('a/text()')[0].extract()
+            item['state'] = club_state
+
+            club_stadium_selector = club_selector_information[3]
+            club_stadium = club_stadium_selector.xpath('a/@title')[0].extract()
+            item['stadium'] = club_stadium
+
+            club_establish_selector = club_selector_information[4]
+            club_establish = club_establish_selector.xpath('text()')[0].extract()
+            item['establish'] = club_establish
+            
+            yield item
+
 
 
         print("--------------------------------")
